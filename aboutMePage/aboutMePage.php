207076a984b062b2c0c98c67df0fadf79b36075a
jaholt <holt.ja.halfways@gmail.com>
2018-10-03 15:12:59 +0100

    My site and the navigation updated to be php to allow for dynamic additions to the display

---
+++ aboutMePage/aboutMePage.php
@@ -0,0 +1,75 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../normalize.css" type="text/css" />
+    <title>Joseph:Home</title>
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+</head>
+<body>
+<nav>
+    <div class="navContainer">
+        <div class="logoName">
+            JaHolt
+        </div>
+        <ul>
+            <a href="../contactPage/contactPage.php"><li>CONTACT</li></a>
+            <a href="../portfolioPage/portfolioPage.php"><li>PORTFOLIO</li></a>
+            <a href="aboutMePage.php"><li>ABOUT</li></a>
+            <a href="../index.php"><li>HOME</li></a>
+        </ul>
+        <div class="navAlignBox">
+        </div>
+    </div>
+</nav>
+<main>
+    <section class="titleContainer">
+        <div class="titleBox">
+            <img src="css/pics/lightbulb.png" alt="lightbulb logo" />
+            <h1>About Me</h1>
+        </div>
+        <div class="aboutMeBox">
+            <h3>About Me</h3>
+            <p>Once completed I will have the fundamentals of coding, software design and agile project management. Complimented by my previous experience in other industries I will have the ability to apply these skills in real world programming situations.</p>
+        </div>
+    </section>
+    <section class="subcategoryContainer">
+        <div class="aboutMeBoxSmall">
+            <h3>About Me</h3>
+            <p>Once completed I will have the fundamentals of coding, software design and agile project management. Complimented by my previous experience in other industries I will have the ability to apply these skills in real world programming situations.</p>
+        </div>
+        <div class="technologiesBox">
+            <h3>Technologies</h3>
+            <p>I will be able to successfully handle and apply the following technologies:</p>
+            <div>
+                <img class="htmlCssImg" src="CSS/pics/htmlCss.png" alt="HTML/CSSimage">
+                <img src="CSS/pics/nodejs.ico" alt="nodeJSLogo">
+                <img src="CSS/pics/php.png" alt="sqlLogo">
+                <img src="CSS/pics/js.png" alt="javascriptLogo">
+            </div>
+        </div>
+        <div class="otherPrevExpContainer">
+            <div class="prevExpBox">
+                <div class="backgroundFade">
+                    <h3 class="titleMaintainControl">Previous Experience</h3>
+                    <p>I have experience working in other industries. I spent some time working for a Executive Recruitment firm based in Westminster that mainly operated in the charity and education sectors.</p>
+                </div>
+            </div>
+            <div class="otherExpBox">
+                <div class="backgroundFade">
+                    <h3 class="titleMaintainControl">Other Experience</h3>
+                    <p>I attained a BA in History from Royal Holloway, University of London. I also have experience working in the Army Reserves where I qualified as a trooper in the Honourable Artillery Company.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="footerBox2">
+            <h4>JaHolt</h4>
+            <h5>&copy; 2018 JaHolt Web Developer</h5>
+        </div>
+    </footer>
+</main>
+</body>
+</html>
